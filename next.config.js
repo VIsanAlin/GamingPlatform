d2cdf3fe00f6126e537a0ccdb1b4338c4c82cb4d
Visan <visan.alinbogdan@gmail.com>
2023-10-22 22:38:02 +0300

    debug

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@
 const nextConfig = {};
 
 module.exports = {
+  env: {
+    AUTH0_SECRET: "use [openssl rand -hex 32] to generate a 32 bytes value",
+    AUTH0_BASE_URL: "http://localhost:3000",
+    AUTH0_ISSUER_BASE_URL: "https://dev-38yuyxf4eh45cx16.us.auth0.com",
+    AUTH0_CLIENT_ID: "dXmzoVVLo1RCxstjf34PiBYa7LTYMBG5",
+    AUTH0_CLIENT_SECRET:
+      "5KASlLUo4TWqHlMoXrHYnu4_pHd6IrBxO-MkdDiCHPuUkFU4AlfiWH67QEfhhg90",
+  },
   poweredByHeader: false,
 };
